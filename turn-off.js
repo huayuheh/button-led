c979fc8d12c5999750d8e001fe1966c7e1523272
hanaheh <hanaheh128@gmail.com>
2016-10-05 23:37:40 -0700

    fix turn off not work

--- turn-off.js
+++ turn-off.js
@@ -3,10 +3,9 @@ var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
   led = new Gpio(27, 'out'),      //setup GPIO27 as output
   ledState = 0; 		  //internal variable to track LED state (1 = on, 0 = off)
 
-setInterval( function(){    //setInterval repeats a function every fixed preset milliseconds
-  led.writeSync( ledState );    //output next ledState
-  ledState = ledState ? 0 : 1;    //update next ledState, if 1 then 0 else if 0 then 1
-}, 3000);        //setInterval fixed preset milliseconds
+var iv = setInterval(function(){
+  led.writeSync(led.readSync() === 0 ? 1 : 0)
+}, 3000);
 
 button.setActiveLow( true );		//optional to reverse button value
 
@@ -16,7 +15,11 @@ button.watch(function(err, value) {	//watch button changes
 	if (value == true){
 		console.log('Button is ON' );
 	}else{
-		led.writeSync( 0 );
+    setTimeout(function() {
+      clearInterval(iv); // Stop blinking
+      led.writeSync(0);  // Turn LED off.
+      led.unexport();    // Unexport GPIO and free resources
+  }, 1);
 		console.log('Button is OFF');
 	}
 });
