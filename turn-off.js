1a944f4edf7fa304072e93627d5c4fde5a0a53b8
hanaheh <hanaheh128@gmail.com>
2016-10-06 19:28:06 -0700

    testing Date function

--- turn-off.js
+++ turn-off.js
@@ -3,7 +3,7 @@ var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
   led = new Gpio(27, 'out'),      //setup GPIO27 as output
   ledState = 0; 		  //internal variable to track LED state (1 = on, 0 = off)
 
-var startTime = new Date(milliseconds);
+var startTime = new Date();
   setTimeout(function(){ led.writeSync( 0 ); }, 3000);
   setTimeout(function(){ led.writeSync( 1 ); }, 6000);
 
@@ -13,8 +13,8 @@ button.setActiveLow( true );		//optional to reverse button value
 
 button.watch(function(err, value) {	//watch button changes
 	if (value == true){
-    endTime = new Date(milliseconds);
-    reactionTime = endTime - startTime;
+    endTime = new Date();
+    reactionTime = endTime.getMilliseconds() - startTime.getMilliseconds();
     led.writeSync( 0 );
 		console.log('Your reaction time is ' + reactionTime + 'ms' );
 	}else{
