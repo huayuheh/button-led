98070e28fa9f0e4b0f5bf39c90ca7b06d84876c6
hanaheh <hanaheh128@gmail.com>
2016-10-06 19:20:54 -0700

    reaction game

--- turn-off.js
+++ turn-off.js
@@ -1,11 +1,11 @@
 var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
   button = new Gpio(17, 'in', 'both'),	//setup GPIO17 as output
   led = new Gpio(27, 'out'),      //setup GPIO27 as output
+  startTime = new Date(milliseconds),
   ledState = 0; 		  //internal variable to track LED state (1 = on, 0 = off)
 
-var iv = setInterval(function(){
-  led.writeSync(led.readSync() === 0 ? 1 : 0)
-}, 3000);
+  setTimeout(function(){ led.writeSync( 0 ); }, 3000);
+  setTimeout(function(){ led.writeSync( 1 ); }, 6000);
 
 button.setActiveLow( true );		//optional to reverse button value
 
@@ -13,14 +13,11 @@ button.setActiveLow( true );		//optional to reverse button value
 
 button.watch(function(err, value) {	//watch button changes
 	if (value == true){
-		console.log('Button is ON' );
+    endTime = new Date(milliseconds);
+    reactionTime = endTime - startTime;
+    led.writeSync( 0 );
+		console.log('Your reaction time is ' + reactionTime + 'ms' );
 	}else{
-    setTimeout(function() {
-      clearInterval(iv); // Stop blinking
-      led.writeSync(0);  // Turn LED off.
-      led.unexport();    // Unexport GPIO and free resources
-  }, 1);
-		console.log('Button is OFF');
 	}
 });
 
