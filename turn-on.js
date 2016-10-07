be4c0c855827365e1202efafb87787bd7f406ef3
hanaheh <hanaheh128@gmail.com>
2016-10-07 01:24:42 -0700

    turn-on & reaction

--- turn-on.js
+++ turn-on.js
@@ -1,19 +1,25 @@
-
 var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
   button = new Gpio(17, 'in', 'both'),	//setup GPIO17 as output
   led = new Gpio(27, 'out'),      //setup GPIO27 as output
   ledState = 0; 		  //internal variable to track LED state (1 = on, 0 = off)
 
-
+var iv = setInterval(function(){
+  led.writeSync(led.readSync() === 0 ? 1 : 0)
+}, 3000);
 
 button.setActiveLow( true );		//optional to reverse button value
 
+
+
 button.watch(function(err, value) {	//watch button changes
 	if (value == true){
-		led.writeSync( 1 );
 		console.log('Button is ON' );
 	}else{
-		led.writeSync( 0 );
+		setTimeout(function() {
+    		clearInterval(iv); // Stop blinking
+    	 	led.writeSync(0);  // Turn LED off.
+    		led.unexport();    // Unexport GPIO and free resources
+  		}, 1);
 		console.log('Button is OFF');
 	}
 });
@@ -23,10 +29,3 @@ process.on('SIGINT', function(){
   led.unexport();
   process.exit();
 });
-
-
-
-
-
-
-
